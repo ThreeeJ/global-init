628a35a77b85a794b3b861dbe806923f09069069
JongJin <2171266@hansung.ac.kr>
2025-07-01 14:03:42 +0900

    feat: 생성 및 수정 시간 자동 기록을 위한 공통 엔티티(BaseEntity) 작성

--- likelion/src/main/java/com/hansung/likelion/LikelionApplication.java
+++ likelion/src/main/java/com/hansung/likelion/LikelionApplication.java
@@ -2,8 +2,10 @@ package com.hansung.likelion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // 이것을 넣어야 시간이 기록이 된다.
 public class LikelionApplication {
 
 	public static void main(String[] args) {
