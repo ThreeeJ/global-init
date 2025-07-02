325be8287b4188e250a9f7e0755b13eb2625c9ea
JongJin <2171266@hansung.ac.kr>
2025-07-02 10:48:57 +0900

    refactor: BaseEntity를 추상 클래스로 변경하여 객체화(인스턴스화) 방지

--- likelion/src/main/java/com/hansung/likelion/global/entity/BaseEntity.java
+++ likelion/src/main/java/com/hansung/likelion/global/entity/BaseEntity.java
@@ -13,7 +13,13 @@ import java.time.LocalDateTime;
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public class BaseEntity {
+public abstract class BaseEntity {
+    // 클래스 -> 추상 클래스로 변경
+    /*
+    - 변경 이유
+        - @MappedSuperclass 때문에 테이블로 매핑되지 않고, 자식 엔티티에게 공통 필드를 상속해주는 역할.
+        - 객체로 만들어질 일이 없음. -> abstract 를 붙여서 new 로 객체가 만들어지는 것을 "미연에 방지"
+    */
 
     @CreatedDate
     @Column(name = "created_at")
